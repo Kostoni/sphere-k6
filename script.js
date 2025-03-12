1ae92c6dc87a3c2305fe0c3614d68830687abef1
Maksim Nabiulin <mnabiulin@gmail.com>
2025-03-12 18:41:05 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,41 @@
+import http from 'k6/http';
+
+const TARGET_HOST = __ENV.TARGET_HOST;
+
+export default function () {
+  let headers = {
+    'Content-Type': 'application/json',
+  }
+
+  let res = http.get(`http://${TARGET_HOST}/auth`, {
+    headers: headers,
+  });
+  
+  const token = res.json('token');
+
+  headers = {
+    'Content-Type': 'application/json',
+    'Authorization': token
+  };
+
+  const body = { 
+    'userName': 'Test user',
+    'userMail': 'testuser@mail.com',
+  };
+
+  res = http.post(`http://${TARGET_HOST}/post`, JSON.stringify(body), {
+    headers: headers,
+  });
+
+  // Задание 6
+
+  if ("members" in res.json() && Array.isArray(res.json("members"))) {
+    for (let item of res.json("members")) {
+      console.log(item["id"]);
+    }
+  } else {
+    let randomKey = Object.keys(res.json())[Math.floor(Object.keys(Math.random()*res.json()).length)];
+
+    console.log(randomKey, res.json(randomKey));
+  }
+}
